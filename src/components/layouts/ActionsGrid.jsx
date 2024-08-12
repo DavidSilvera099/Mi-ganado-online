2743c5d970521ab20b91c0281f20b8792ea5af34
David Silvera <david4709djsr@gmail.com>
2024-08-12 16:44:56 -0500

    Corrigiendo ruta en actionsGrid

--- src/components/layouts/ActionsGrid.jsx
+++ src/components/layouts/ActionsGrid.jsx
@@ -5,12 +5,12 @@ const ActionsGrid = () => {
     return (
         <div className="my-8">
             <div className="flex justify-center items-center gap-8">
-                <a href='/AdminAnimals'>
+                <Link href='/AdminAnimals'>
                     <div className="flex items-center flex-col gap-3 w-52 h-38 p-4 text-wrap shadow-md">
                         <i className="fa-solid fa-gear text-4xl text-VerdeOscuro"></i>
                         <h3 className="text-md md:text-lg lg:text-xl xl:text-2xl text-VerdeOscuro font-bold" >Editar/Eliminar</h3>
                     </div>
-                </a>
+                </Link>
                 <Link to='/AnimalForm'>
                     <div className="flex items-center flex-col gap-3 w-52 h-38 p-4 shadow-md">
                         <i className="fa-solid fa-plus text-4xl text-VerdeOscuro"></i>
