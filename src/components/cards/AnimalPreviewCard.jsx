47ea8a82564ef921d2aa2a5f2d9d957eeed8517f
David Silvera <david4709djsr@gmail.com>
2024-08-12 16:42:54 -0500

    ajustando previewcard

--- src/components/cards/AnimalPreviewCard.jsx
+++ src/components/cards/AnimalPreviewCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const AnimalPreviewCard = ({ animal }) => {
     return (
-        <Link className='flex w-4/5 h-40 md:w-4/5 border border-gray-200 rounded-lg shadow-lg overflow-hidden m-4' to={`/animals/${animal.id}`}>
+        <Link className='flex w-11/12 h-40 md:w-4/5 border border-gray-200 rounded-lg shadow-lg overflow-hidden m-4' to={`/animals/${animal.id}`}>
             <div className='w-5/6'>
                 <img src={animal.photoUrl || 'https://via.placeholder.com/150'} alt={animal.name} className="object-cover h-full w-full" />
             </div>
