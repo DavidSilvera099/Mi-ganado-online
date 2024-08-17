9d815743949bc6b74cdb5fb6ea505d730c4cc845
david <david4709djsr@gmail.com>
2024-08-17 15:31:10 -0500

    Corrijo posicion de footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-VerdeMusgo text-white py-6 fixed bottom-0 w-full">
+        <footer className="bg-VerdeMusgo text-white py-6 w-full">
             <div className="container mx-auto text-center">
                 <p className="text-sm">
                     ©{new Date().getFullYear()} Creado por <span className='font-semibold'>David Silvera</span> Todos los derechos reservados.
